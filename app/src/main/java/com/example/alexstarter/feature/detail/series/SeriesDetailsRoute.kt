07750866b4b59f8b9be51e57774d8d80d4e97d25
AlexQobuz <alexandre.clemencot@qobuz.com>
2024-11-18 16:36:10 +0100

    Change UI from details screen for series

--- app/src/main/java/com/example/alexstarter/feature/detail/series/SeriesDetailsRoute.kt
+++ app/src/main/java/com/example/alexstarter/feature/detail/series/SeriesDetailsRoute.kt
@@ -1,8 +1,11 @@
 package com.example.alexstarter.feature.detail.series
 
+import android.util.Log
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
@@ -14,6 +17,11 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawWithContent
+import androidx.compose.ui.graphics.BlendMode
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,7 +32,9 @@ import com.example.alexstarter.designsystem.Spacer
 import com.example.alexstarter.designsystem.appbar.TopBar
 import com.example.alexstarter.designsystem.message.ErrorMessage
 import com.example.alexstarter.designsystem.text.Text
+import com.example.alexstarter.designsystem.text.Title
 import com.example.alexstarter.designsystem.text.TitleWithRow
+import com.example.alexstarter.ui.theme.DarkBlue
 
 @Composable
 fun SeriesDetailRoute(
@@ -62,6 +72,7 @@ fun SeriesDetailScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .verticalScroll(rememberScrollState())
+                .background(color = DarkBlue)
         ) {
             when (seriesDetailsState) {
                 is SeriesDetailsState.Error -> {
@@ -79,29 +90,50 @@ fun SeriesDetailScreen(
                     val progress = viewModel.convertRatingToProgress(seriesPopularity)
                     val series = seriesDetailsState.series
 
-                    AsyncImage(
-                        modifier = Modifier.height(350.dp),
-                        model = series.posterPath,
-                        contentScale = ContentScale.Crop,
-                        contentDescription = "image of ${series.name}"
-                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth(),
+                        contentAlignment = Alignment.BottomStart
+                    ) {
+                        AsyncImage(
+                            modifier = Modifier
+                                .height(350.dp)
+                                .graphicsLayer { alpha = 0.99f }
+                                .drawWithContent {
+                                    val colors = listOf(
+                                        Color.Black,
+                                        Color.Transparent
+                                    )
+                                    drawContent()
+                                    drawRect(
+                                        brush = Brush.verticalGradient(colors),
+                                        blendMode = BlendMode.DstIn
+                                    )
+                                },
+                            model = series.posterPath,
+                            contentScale = ContentScale.Crop,
+                            contentDescription = "image of ${series.name}"
+                        )
+                        TitleWithRow(text = series.name)
+                    }
                     Column(modifier = Modifier.padding(8.dp)) {
 
-                        TitleWithRow(text = series.name)
                         Spacer.Vertical.Default()
+                        Text.Default(text = "Description :")
+                        Spacer.Vertical.Small()
                         Text.Default(text = series.overview)
-                        Spacer.Vertical.Small()
-                        series.type?.let { Text.Default(text = series.type) }
-                        Spacer.Vertical.Small()
-                        series.status?.let { Text.Default(text = series.status) }
-                        Spacer.Vertical.Small()
-                        Text.Default(text = series.firstAirDate)
-                        Spacer.Vertical.Small()
-                        series.homepage?.let { Text.Default(text = it) }
-                        Spacer.Vertical.Small()
-                        Text.Default(text = series.originalLanguage)
-                        Spacer.Vertical.Small()
-                        series.tagline?.let { Text.Default(text = series.tagline) }
+                        Spacer.Vertical.Default()
+                        series.type?.let { Text.Default(text = "Type : ${series.type}") }
+                        Spacer.Vertical.Default()
+                        series.status?.let { Text.Default(text = "Status : ${series.status}") }
+                        Spacer.Vertical.Default()
+                        Text.Default(text = "first air date : ${series.firstAirDate}")
+                        Spacer.Vertical.Default()
+                        series.homepage?.let { Text.Default(text = "Homepage : $it") }
+                        Spacer.Vertical.Default()
+                        Text.Default(text = "Originale language : ${series.originalLanguage}")
+                        Spacer.Vertical.Default()
+                        series.tagline?.let { Text.Default(text = "Tagline : ${series.tagline}") }
                     }
                 }
                 SeriesDetailsState.Loading -> {
